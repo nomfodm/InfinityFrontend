827b7e622999e05eb0ae9fb99862027e2eca0feb
alex <nomfodm@gmail.com>
2025-01-12 13:15:49 +0300

    Add: axios response time to prevent app stuck

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 export const $api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
     withCredentials: true,
-    headers: {"Content-Type": "application/json"}
+    headers: {"Content-Type": "application/json"},
+    timeout: 3000,
 })
